224832af704985865973c0b7164f296a334e0af1
boburmirzo <boburmirzo.umurzokov@gmail.com>
2023-08-05 20:14:40 +0300

    delete test code

--- examples/pipelines/contextful/app.py
+++ examples/pipelines/contextful/app.py
@@ -1,30 +1,3 @@
-"""
-Microservice for  a context-aware ChatGPT assistant.
-
-The following program reads in a collection of documents,
-embeds each document using the OpenAI document embedding model,
-then builds an index for fast retrieval of documents relevant to a question,
-effectively replacing a vector database.
-
-The program then starts a REST API endpoint serving queries about programming in Pathway.
-
-Each query text is first turned into a vector using OpenAI embedding service,
-then relevant documentation pages are found using a Nearest Neighbor index computed
-for documents in the corpus. A prompt is build from the relevant documentations pages
-and sent to the OpenAI GPT-4 chat service for processing.
-
-Usage:
-In the root of this repository run:
-`poetry run ./run_examples.py contextful`
-or, if all dependencies are managed manually rather than using poetry
-`python examples/pipelines/contextful/app.py`
-
-You can also run this example directly in the environment with llm_app instaslled.
-
-To call the REST API:
-curl --data '{"user": "user", "query": "How to connect to Kafka in Pathway?"}' http://localhost:8080/ | jq
-"""
-
 import os
 
 import pathway as pw
@@ -33,42 +6,101 @@ from pathway.stdlib.ml.index import KNNIndex
 from llm_app.model_wrappers import OpenAIChatGPTModel, OpenAIEmbeddingModel
 
 
-class DocumentInputSchema(pw.Schema):
-    doc: str
+class DiscountsInputSchema(pw.Schema):
+    ship_date: str
+    ship_mode: str
+    country: str
+    city: str
+    state: str
+    postal_code: str
+    region: str
+    product_id: str
+    category: str
+    sub_category: str
+    brand: str
+    product_name: str
+    currency: str
+    actual_price: str
+    discount_price: str
+    discount_percentage: str
+    address: str
 
 
 class QueryInputSchema(pw.Schema):
     query: str
     user: str
 
+def concat_with_titles(*args) -> str:
+    titles = [
+        "country",
+        "city",
+        "ship_mode",
+        "state",
+        "product_id",
+        "postal_code",
+        "region",
+        "category",
+        "sub_category",
+        "brand",
+        "product_name",
+        "actual_price",
+        "discount_price",
+        "discount_percentage",
+        "address",
+        "currency",
+        "ship_date",
+    ]
+    combined = [f"{title}: {value}" for title, value in zip(titles, args)]
+    return ', '.join(combined)
 
 def run(
     *,
-    data_dir: str = os.environ.get("PATHWAY_DATA_DIR", "./examples/data/pathway-docs/"),
+    data_dir: str = os.environ.get("PATHWAY_DATA_DIR", "./examples/find_discounts/data/"),
     api_key: str = os.environ.get("OPENAI_API_TOKEN", ""),
     host: str = "0.0.0.0",
     port: int = 8080,
     embedder_locator: str = "text-embedding-ada-002",
     embedding_dimension: int = 1536,
     model_locator: str = "gpt-3.5-turbo",
-    max_tokens: int = 60,
+    max_tokens: int = 200,
     temperature: int = 0.0,
     **kwargs,
 ):
     embedder = OpenAIEmbeddingModel(api_key=api_key)
 
-    documents = pw.io.jsonlines.read(
+    sales_data = pw.io.csv.read(
         data_dir,
-        schema=DocumentInputSchema,
+        schema=DiscountsInputSchema,
         mode="streaming",
         autocommit_duration_ms=50,
     )
 
-    enriched_documents = documents + documents.select(
-        data=embedder.apply(text=pw.this.doc, locator=embedder_locator)
+    combined_data = sales_data.select(
+        doc=pw.apply(concat_with_titles,
+                           pw.this.country,
+                           pw.this.city,
+                           pw.this.ship_mode,
+                           pw.this.state,
+                           pw.this.product_id,
+                           pw.this.postal_code,
+                           pw.this.region,
+                           pw.this.category,
+                           pw.this.sub_category,
+                           pw.this.brand,
+                           pw.this.product_name,
+                           pw.this.actual_price,
+                           pw.this.discount_price,
+                           pw.this.discount_percentage,
+                           pw.this.address,
+                           pw.this.currency,
+                           pw.this.ship_date),
     )
 
-    index = KNNIndex(enriched_documents, d=embedding_dimension)
+    enriched_data = combined_data + combined_data.select(
+        data=embedder.apply(text=combined_data.doc, locator=embedder_locator)
+    )
+
+    index = KNNIndex(enriched_data, d=embedding_dimension)
 
     query, response_writer = pw.io.http.rest_connector(
         host=host,
@@ -82,17 +114,17 @@ def run(
     )
 
     query_context = index.query(query, k=3).select(
-        pw.this.query, documents_list=pw.this.result
+        pw.this.query, local_indexed_data_list=pw.this.result
     )
 
     @pw.udf
-    def build_prompt(documents, query):
-        docs_str = "\n".join(documents)
-        prompt = f"Given the following documents : \n {docs_str} \nanswer this query: {query}"
+    def build_prompt(local_indexed_data, query):
+        docs_str = "\n".join(local_indexed_data)
+        prompt = f"Given the following discounts data: \n {docs_str} \nanswer this query: {query} and clean the output"
         return prompt
 
     prompt = query_context.select(
-        prompt=build_prompt(pw.this.documents_list, pw.this.query)
+        prompt=build_prompt(pw.this.local_indexed_data_list, pw.this.query)
     )
 
     model = OpenAIChatGPTModel(api_key=api_key)
--- run_examples.py
+++ run_examples.py
@@ -122,14 +122,6 @@ def contextful(**kwargs):
 
 @cli.command()
 @common_options
-def findDiscounts(**kwargs):
-    from examples.find_discounts import run
-
-    return run(**kwargs)
-
-
-@cli.command()
-@common_options
 def contextful_s3(**kwargs):
     from examples.pipelines.contextful_s3 import run
 
