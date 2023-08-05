89540895b1433f51255f0a1d70c35fda1d77a5ea
boburumurzokov <boburmirzo.umurzokov@gmail.com>
2023-08-05 20:39:33 +0300

    Add find discounts example

--- run_examples.py
+++ run_examples.py
@@ -122,6 +122,14 @@ def contextful(**kwargs):
 
 @cli.command()
 @common_options
+def find_discounts(**kwargs):
+    from examples.pipelines.find_discounts import run
+
+    return run(**kwargs)
+
+
+@cli.command()
+@common_options
 def contextful_s3(**kwargs):
     from examples.pipelines.contextful_s3 import run
 
